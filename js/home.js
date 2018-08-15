23ab998b8fa63f10468e23d2c7039d075bb6dc48
hisuich <david.barabash.2014@gmail.com>
2018-08-15 17:29:41 +0300

    udpate first task

--- js/home.js
+++ js/home.js
@@ -1,10 +1,11 @@
 window.onload = function() {
     const rows = document.getElementsByClassName("main")[0].getElementsByClassName("row");
     node = rows[rows.length - 1];
-    node.style.height = "75vh";
+    node.style.maxHeight = "75vh";
+    console.log(node.style);
 
     const hideOpen = () => {
-        node.style.height == "150vh" ? node.style.height = "75vh" : node.style.height = "150vh";
+        node.style.maxHeight == "150vh" ? node.style.maxHeight = "75vh" : node.style.maxHeight = "150vh";
     }
     
     node.onclick = hideOpen;
